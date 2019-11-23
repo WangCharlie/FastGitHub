de8d4b8d7acf5e32bf0b636f23fb604fbb9814ba
Charlie Wang <cheng-linw@hotmail.com>
2019-11-23 22:18:47 +0800

    init

---
+++ src/FastGitHub/FastGitHub/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace FastGitHub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var OSInfo = Environment.OSVersion;
+            string pathpart = "hosts";
+            if (OSInfo.Platform == PlatformID.Win32NT)
+            {
+                //is windows NT
+                pathpart = "system32\\drivers\\etc\\hosts";
+            }
+            string hostfile = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Windows), pathpart);
+
+
+            const string tales = "123.123.123.123 download.talesrunner.com";
+            string[] lines = File.ReadAllLines(hostfile);
+
+            if (lines.Any(s => s.Contains("download.talesrunner.com")))
+            {
+                for (int i = 0; i < lines.Length; i++)
+                {
+                    if (lines[i].Contains("download.talesrunner.com"))
+                        lines[i] = tales;
+                }
+                File.WriteAllLines(hostfile, lines);
+            }
+            else if (!lines.Contains(tales))
+            {
+                File.AppendAllLines(hostfile, new String[] { tales });
+            }
+        }
+    }
+}
